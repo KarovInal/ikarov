3394996badcc12bbdc40cab7076d14586a49fc3a
KarovInal <bagov.ali07@gmail.com>
2018-01-19 17:54:41 +0300

    setup env

---
+++ public/dist/pages/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function () {
+  return _react2.default.createElement('h1', {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, 'Welcome to my sites!');
+};
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = 'C:\\Users\\Inal_Karov\\site\\ikarov\\pages\\index.js?entry';
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJSZWFjdCJdLCJtYXBwaW5ncyI6Ijs7Ozs7O2tCQUVlLFlBQVcsQUFDeEI7eUJBQ0UsY0FBQTs7Z0JBQUE7a0JBQUE7QUFBQTtBQUFBLEdBQUEsRUFERixBQUNFLEFBSUg7OztBQVJELEFBQU8sQUFFUCIsImZpbGUiOiJpbmRleC5qcz9lbnRyeSIsInNvdXJjZVJvb3QiOiJDOi9Vc2Vycy9JbmFsX0thcm92L3NpdGUvaWthcm92In0=
